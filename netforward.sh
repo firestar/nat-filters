d8eee428056ad4ddca827c24128bd80921ed45a3
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2021-11-13 12:13:26 -0800

    Update netforward.sh

--- netforward.sh
+++ netforward.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-echo "\t Setting up ip forwarding for 192.168.122.x"
+#
+##
+InDev=enp4s0
+OutDev=virbr0
+Network=192.168.122.0/24
+##
+#
+
+echo "\t Setting up ip forwarding for ${Network} from ${InDev} to ${OutDev}"
+
 modprobe iptable_nat
 modprobe ip_conntrack
 echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables -t nat -A POSTROUTING -s 192.168.122.0/24 -j MASQUERADE
-iptables -A FORWARD -o virbr0 -i enp4s0 -s 192.168.122.0/24 -m conntrack --ctstate ESTABLISHED -j ACCEPT
-iptables -A FORWARD -m conntrack --ctstate NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -t nat -A POSTROUTING -s $Network -j MASQUERADE
+iptables -A FORWARD -o $OutDev -i $InDev -s $Network -m conntrack --ctstate NEW -j ACCEPT
+iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 sleep 25
-
+# Remove rule automatically created to block this...
 iptables -D LIBVIRT_FWI -o virbr0 -j REJECT --reject-with icmp-port-unreachable
